2fa063c58475bad7abd65944f61e813fc4240cb7
Marcela <marcelaperez@gmail.com>
2024-03-17 17:18:39 +0100

    notebook to py

--- 05-batch/Homework.py
+++ 05-batch/Homework.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
-
-
 import pyspark
 from pyspark.sql import SparkSession
-
-
-# In[43]:
+import pandas as pd
+from pyspark.sql import types
+from pyspark.sql import functions as F
 
 
 spark = SparkSession.builder     .master("local[*]")     .appName('test')     .getOrCreate()
@@ -28,80 +25,24 @@ df_zone.show()
 # Execute spark.version.
 # What's the output?
 
-# In[5]:
-
-
 spark.version
-
-
-# In[7]:
-
 
 get_ipython().system('wget https://github.com/DataTalksClub/nyc-tlc-data/releases/download/fhv/fhv_tripdata_2019-10.csv.gz')
 
-
-# In[16]:
-
-
 get_ipython().system('gzip -dc fhv_tripdata_2019-10.csv.gz > fhv_tripdata_2019-10.csv')
-
-
-# In[17]:
-
 
 get_ipython().system('wc -l fhv_tripdata_2019-10.csv')
 
-
-# In[18]:
-
-
 df = spark.read     .option("header", "true")     .csv('fhv_tripdata_2019-10.csv')
 
-
-# In[19]:
-
-
 df.schema
-
-
-# In[20]:
-
 
 get_ipython().system('head -n 1001 fhv_tripdata_2019-10.csv > head.csv')
 
 
-# In[23]:
-
-
-import pandas as pd
-
-
-# In[24]:
-
-
 df_pandas = pd.read_csv('head.csv')
-
-
-# In[25]:
-
-
 df_pandas.dtypes
-
-
-# In[26]:
-
-
 spark.createDataFrame(df_pandas).schema
-
-
-# In[27]:
-
-
-from pyspark.sql import types
-
-
-# In[28]:
-
 
 schema = types.StructType([
     types.StructField('dispatching_base_num', types.StringType(), True),
@@ -124,46 +65,16 @@ schema = types.StructType([
 # 
 # What is the average size of the Parquet (ending with .parquet extension) Files that were created (in MB)? Select the answer which most closely matches.
 
-# In[30]:
-
-
 df = df.repartition(6)
-
-
-# In[31]:
-
-
 df.write.parquet('fhvhv/2019/10/')
-
 
 # Each partition has approx. 6Mb
 
-# In[32]:
-
-
 df = spark.read.parquet('fhvhv/2019/10/')
-
-
-# In[33]:
-
 
 df.printSchema()
 
-
-# In[34]:
-
-
-from pyspark.sql import functions as F
-
-
-# In[35]:
-
-
 df.show()
-
-
-# In[36]:
-
 
 df.printSchema()
 
@@ -176,37 +87,15 @@ df.printSchema()
 # 
 # Consider only trips that started on the 15th of October.
 
-# In[39]:
-
-
-from pyspark.sql import SparkSession
-from pyspark.sql import functions as F
-
-
-# In[58]:
-
-
 df.registerTempTable('fhv_data')
 
-
-# In[60]:
-
-
 spark.sql("SELECT * FROM fhv_data LIMIT 10;").show()
-
-
-# In[63]:
-
 
 sql_query = """
 SELECT COUNT(*)
 FROM fhv_data
 WHERE DATE(pickup_datetime) = '2019-10-15'
 """
-
-
-# In[64]:
-
 
 result = spark.sql(sql_query)
 result.show()
@@ -218,18 +107,12 @@ result.show()
 # 
 # What is the length of the longest trip in the dataset in hours?
 
-# In[65]:
-
-
 sql_query = """
 SELECT
     MAX(TIMESTAMPDIFF(HOUR, pickup_datetime, dropoff_datetime)) AS longest_trip_hours
 FROM
     fhv_data
 """
-
-
-# In[66]:
 
 
 result = spark.sql(sql_query)
@@ -254,31 +137,16 @@ result.show()
 # 
 # Using the zone lookup data and the FHV October 2019 data, what is the name of the LEAST frequent pickup location Zone?
 
-# In[29]:
-
 
 df = spark.read     .option("header", "true")     .schema(schema)     .csv('fhv_tripdata_2019-10.csv')
 
 
-# In[44]:
-
-
 df_pandas2 = pd.read_csv('taxi+_zone_lookup.csv')
-
-
-# In[45]:
-
 
 df_pandas2.dtypes
 
 
-# In[46]:
-
-
 spark.createDataFrame(df_pandas2).schema
-
-
-# In[47]:
 
 
 schema2 = types.StructType([
@@ -289,43 +157,15 @@ schema2 = types.StructType([
 ])
 
 
-# In[49]:
-
-
 df_zone = spark.read     .option("header", "true")     .schema(schema2)     .csv('taxi+_zone_lookup.csv')
-
-
-# In[50]:
-
-
 df_zone.show()
-
-
-# In[52]:
-
-
 df_zone.write.parquet('taxi+_zone_lookup')
-
-
-# In[53]:
-
-
 df_zone = spark.read.parquet('taxi+_zone_lookup')
-
-
-# In[54]:
-
 
 df_zone.printSchema()
 
-
-# In[67]:
-
-
 df_zone.registerTempTable('fhv_zone')
 
-
-# In[68]:
 
 
 sql_query = """
@@ -345,15 +185,8 @@ ORDER BY
 LIMIT 1
 """
 
-
-# In[69]:
-
-
 result = spark.sql(sql_query)
 result.show()
-
-
-# In[ ]:
 
 
 
